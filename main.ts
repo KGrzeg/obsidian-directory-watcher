2558b2254702f3d499ae842b958bcc10ca818e6a
KGrzeg <kupczykgrzeg@gmail.com>
2021-10-08 21:56:23 +0200

    Extractr NoteUpdater class

--- main.ts
+++ main.ts
@@ -6,7 +6,8 @@ import {
 	Setting,
 	TFile,
 	TFolder,
-	debounce
+	debounce,
+	moment
 } from 'obsidian';
 
 import { ExifImage, ExifData } from 'exif';
@@ -48,17 +49,23 @@ const DEFAULT_SETTINGS: DirectoryWatcherSettings = {
 class ImageFile {
 	file: TFile;
 	meta: ExifData;
+	containMeta: boolean;
 
 	constructor(file: TFile, meta: ExifData) {
 		this.file = file;
 		this.meta = meta;
+
+		this.containMeta = !!this.meta;
 	}
 
 	getNoteLine(): string {
 		let displayName = this.file.name;
 
 		if (this.meta?.exif?.CreateDate) {
-			displayName += " created at " + this.meta.exif.CreateDate;
+			const exifToMomentFormat = "YYYY:MM:DD hh:mm:ss";
+			const date = moment(this.meta.exif.CreateDate, exifToMomentFormat);
+
+			displayName += " created at " + date.format(moment.HTML5_FMT.DATE);
 		}
 
 		return `- ![${displayName}](${this.file.name})`
@@ -81,22 +88,85 @@ class ImageFile {
 	}
 }
 
+class NoteUpdater {
+	filesToAdd: ImageFile[];
+	update: Function;
+	basePath: string;
+	plugin: DirectoryWatcher;
+
+	get app() { return this.plugin.app };
+	get settings() { return this.plugin.settings };
+
+	constructor(plugin: DirectoryWatcher, basePath: string) {
+		this.filesToAdd = [];
+
+		this.plugin = plugin;
+		this.basePath = basePath;
+
+		this.update = debounce(this.executeUpdate, 500, true);
+	}
+
+	async addFile(file: TFile) {
+		this.filesToAdd.push(
+			await ImageFile.create(file, this.basePath)
+		);
+		
+		this.update();
+	}
+
+	async executeUpdate() {
+		const filesToAdd = this.filesToAdd;
+		this.filesToAdd = [];
+
+		log(`Execute updating with `, filesToAdd);
+
+		const noteToUpdate = this.app.vault.getAbstractFileByPath(this.settings.noteToUpdate);
+		if (noteToUpdate instanceof TFile) {
+			const previousContent = await this.app.vault.read(noteToUpdate);
+			const content = this.prepareContent(previousContent, filesToAdd);
+
+			await this.app.vault.modify(
+				noteToUpdate,
+				content
+			);
+
+			log(`Updated ${noteToUpdate.path} with ${filesToAdd.length} lines`);
+		} else {
+			console.error("The note to update do not exists");
+		}
+
+	}
+
+	prepareContent(previousContent: string, filesToAdd: ImageFile[]) {
+		const appendLines = filesToAdd
+			.map(file => file.getNoteLine())
+			.join('\n');
+
+		const newContent = previousContent + '\n' + appendLines;
+
+		//sort and remove doubles
+		return newContent
+			.split('\n')
+			.sort((a, b) => a.localeCompare(b))
+			//remove duplicates taking advantage from sorted lines
+			.filter((item, pos, arr) => !pos || item != arr[pos - 1])
+			.join('\n');
+	}
+}
+
 export default class DirectoryWatcher extends Plugin {
 	settings: DirectoryWatcherSettings;
-	filesToAdd: ImageFile[];
-	updateNote: any;
 	basePath: string;
+	updater: NoteUpdater;
 
 	async onload() {
 		log('loading plugin');
 
 		this.basePath = (this.app.vault.adapter as any).basePath;
+		this.updater = new NoteUpdater(this, this.basePath);
 
 		await this.loadSettings();
 		log('The settings:', this.settings);
-
-		this.filesToAdd = [];
-		this.updateNote = debounce(this.updateNote_raw, 500, true);
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -105,7 +175,7 @@ export default class DirectoryWatcher extends Plugin {
 		this.registerEvent(this.app.vault.on("create", newImage => {
 			if (newImage instanceof TFile
 				&& newImage.parent.path == this.settings.directoryToWatch) {
-				this.addToNote(newImage);
+				this.updater.addFile(newImage);
 			}
 		}));
 	}
@@ -158,28 +228,7 @@ export default class DirectoryWatcher extends Plugin {
 		}
 	}
 
-	async addToNote(file: TFile) {
-		const newFile = await ImageFile.create(file, this.basePath);
 
-		this.filesToAdd.push(newFile);
-		this.updateNote();
-	}
-
-	prepareContent(previousContent: string, filesToAdd: ImageFile[]) {
-		const appendLines = filesToAdd
-			.map(file => file.getNoteLine())
-			.join('\n');
-
-		const newContent = previousContent + '\n' + appendLines;
-
-		//sort and remove doubles
-		return newContent
-			.split('\n')
-			.sort((a, b) => a.localeCompare(b))
-			//remove duplicates taking advantage from sorted lines
-			.filter((item, pos, arr) => !pos || item != arr[pos - 1])
-			.join('\n');
-	}
 
 	async updateNote_raw() {
 		const filesToAdd = this.filesToAdd;
